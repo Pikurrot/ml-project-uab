37318d433f331b783f8abe4dafb8abaf093d3975
Eric Lopez <pikurrot@gmail.com>
2023-12-02 17:36:19 +0100

    Define tree importances()

--- src/decisiontree.py
+++ src/decisiontree.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from src.base import Base
 
@@ -29,3 +30,27 @@ class DecisionTree(DecisionTreeClassifier, Base):
 			self,
 			name="Decision Tree",
 			random_state=random_state)
+
+	def importances(self, features: list = None, show: bool = False):
+		"""
+		Compute the feature importances.
+
+		## Parameters
+		features: list. Feature names.
+		show: bool. Whether to show the feature importances plot.
+
+		## Returns
+		importances: if features is provided, a dict with the feature names and their\
+			importances sorted from highest to lowest. Otherwise, a list with the importances.
+		"""
+		if features is None:
+			importances = self.feature_importances_
+		else:
+			importances = dict(zip(features, self.feature_importances_))
+			importances = {k: v for k, v in sorted(importances.items(), key=lambda item: item[1], reverse=True)}
+
+		if show:
+			# bar plot, with feature names is available
+			plt.bar(range(len(importances)), list(importances.values()), align='center')
+			plt.xticks(range(len(importances)), list(importances.keys()), rotation=90)
+			plt.show()
