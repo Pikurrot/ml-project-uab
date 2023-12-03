b0ccc3b41a8f07f5404cb4d74762765ab287b5f9
Eric Lopez <pikurrot@gmail.com>
2023-12-03 20:39:41 +0100

    Define random forest importances

--- src/randomforest.py
+++ src/randomforest.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from src.base import Base
 
@@ -37,3 +38,27 @@ class RandomForest(RandomForestClassifier, Base):
 			self,
 			name="Random Forest",
 			random_state=random_state)
+
+	def importances(self, features: list = None, show: bool = False):
+		"""
+		Compute the feature importances.
+
+		## Parameters
+		features: list. Feature names.
+		show: bool. Whether to show the feature importances plot.
+
+		## Returns
+		importances: if features is provided, a dict with the feature names and their\
+			importances sorted from highest to lowest. Otherwise, a list with the importances.
+		"""
+		if features is None:
+			importances = self.feature_importances_
+		else:
+			importances = dict(zip(features, self.feature_importances_))
+			importances = {k: v for k, v in sorted(importances.items(), key=lambda item: item[1], reverse=True)}
+
+		if show:
+			# bar plot, with feature names is available
+			plt.bar(range(len(importances)), list(importances.values()), align='center')
+			plt.xticks(range(len(importances)), list(importances.keys()), rotation=90)
+			plt.show()
