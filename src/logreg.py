70c4bf036fbbe2c98e4f198761f07bf5c3c838ff
Eric Lopez <pikurrot@gmail.com>
2023-12-10 10:57:22 +0100

    Update LogReg class
    Add more parameter choice

--- src/logreg.py
+++ src/logreg.py
@@ -6,19 +6,29 @@ class LogReg(LogisticRegression, Base):
 				random_state: int = 42,
 				epsilon: float = 1e-4,
 				penalty: str = "l2",
-				max_iter: int = 10000):
+				lam: float = 1.0,
+				max_iter: int = 10000,
+				class_weight: str = None,
+				solver: str = "lbfgs"):
 		"""
 		Logistic regression model.
 		"""
 
 		self.penalty = penalty
 		self.epsilon = epsilon
+		self.lam = lam
 		self.max_iter = max_iter
+		self.class_weight = class_weight
+		self.solver = solver
+
 		LogisticRegression.__init__(
 			self,
 			penalty = self.penalty,
+			C = 1/lam,
 			tol = self.epsilon,
 			max_iter = self.max_iter,
+			class_weight = class_weight,
+			solver = self.solver,
 			random_state = random_state)
 		
 		Base.__init__(
