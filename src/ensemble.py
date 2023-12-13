4179a35b5535a8d7533c6ae624d33e55b73bfbe5
Eric Lopez <pikurrot@gmail.com>
2023-12-13 23:05:21 +0100

    Define Ensemble call function

--- src/ensemble.py
+++ src/ensemble.py
@@ -1,5 +1,8 @@
 import numpy as np
+import pandas as pd
+from typing import Union
 from src.base import Base
+import src.utils as utils
 
 class Ensemble(Base):
 	def __init__(self,
@@ -65,3 +68,31 @@ class Ensemble(Base):
 			sum = np.sum(hardmax, axis=2) # (n_samples, n_classes)
 			majority = np.argmax(sum, axis=1) # (n_samples,)
 			return majority
+		
+	def __call__(self, X: Union[pd.DataFrame, np.ndarray]) -> Union[pd.Series, np.ndarray]:
+		"""
+		Predict class names for samples in X. Similar to predict,
+		but it takes raw data and preprocesses it.
+
+		## Parameters
+		X: shape (n_samples, n_features)
+
+		## Returns
+		y_pred: shape (n_samples,)
+		"""
+		if isinstance(X, pd.DataFrame):
+			X_copy = X.copy().reset_index(drop=True)
+			Xy = pd.concat([X_copy, pd.DataFrame(np.zeros((X_copy.shape[0], 1)), columns=["cut"])], axis=1)
+			X_new, _ = utils.preprocessing_LS_simple(Xy)
+		elif isinstance(X, np.ndarray):
+			Xy = np.concatenate([X, np.zeros((X.shape[0], 1))], axis=1)
+			X_df = pd.DataFrame(Xy,
+				columns=["Unnamed: 0", "carat", "color", "clarity", "depth", "table", "price", "x", "y", "z", "cut"])
+			X_new, _ = utils.preprocessing_LS_simple(X_df)
+		pred_label = self.predict(X_new)
+		cut_mapping = {4: "Ideal", 3: "Premium", 2: "Very Good", 1: "Good", 0: "Fair"}
+		pred = pd.Series(pred_label).map(cut_mapping)
+		if isinstance(X, pd.DataFrame):
+			return pred
+		elif isinstance(X, np.ndarray):
+			return pred.to_numpy()
