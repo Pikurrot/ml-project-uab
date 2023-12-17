1176592d49f6839d637a7e5a4aafb465888c32cf
Eric Lopez <pikurrot@gmail.com>
2023-12-17 17:24:17 +0100

    Update Ensemble call

--- src/ensemble.py
+++ src/ensemble.py
@@ -8,15 +8,13 @@ class Ensemble(Base):
 	def __init__(self,
 			  	models: list,
 				voting: str = "soft",
-				weights: list = None,
-				scaler=None):
+				weights: list = None):
 		"""
 		Ensemble model.
 		"""
 		self.models = models
 		self.voting = voting
 		self.weights = weights
-		self.scaler = scaler
 
 		super().__init__(
 			name="Ensemble",
@@ -71,32 +69,14 @@ class Ensemble(Base):
 			majority = np.argmax(sum, axis=1) # (n_samples,)
 			return majority
 		
-	def __call__(self, X: Union[pd.DataFrame, np.ndarray]) -> Union[pd.Series, np.ndarray]:
+	def __call__(self, X: np.ndarray) -> np.ndarray:
 		"""
-		Predict class names for samples in X. Similar to predict,
-		but it takes raw data and preprocesses it.
+		Predict class labels for samples in X.
 
 		## Parameters
-		X: shape (n_samples, n_features)
+		X: np.ndarray of shape (n_samples, n_features)
 
 		## Returns
-		y_pred: shape (n_samples,)
+		y_pred: np.ndarray of shape (n_samples,)
 		"""
-		if isinstance(X, pd.DataFrame):
-			if "Unnamed: 0" not in X.columns: # the data has already been processed
-				return self.predict(X)
-			X_copy = X.copy().reset_index(drop=True)
-			Xy = pd.concat([X_copy, pd.DataFrame(np.zeros((X_copy.shape[0], 1)), columns=["cut"])], axis=1)
-			X_new, _ = utils.preprocessing_LS_simple(Xy, scaler=self.scaler)
-		elif isinstance(X, np.ndarray):
-			Xy = np.concatenate([X, np.zeros((X.shape[0], 1))], axis=1)
-			X_df = pd.DataFrame(Xy,
-				columns=["Unnamed: 0", "carat", "color", "clarity", "depth", "table", "price", "x", "y", "z", "cut"])
-			X_new, _ = utils.preprocessing_LS_simple(X_df, scaler=self.scaler)
-		pred_label = self.predict(X_new)
-		cut_mapping = {4: "Ideal", 3: "Premium", 2: "Very Good", 1: "Good", 0: "Fair"}
-		pred = pd.Series(pred_label).map(cut_mapping)
-		if isinstance(X, pd.DataFrame):
-			return pred
-		elif isinstance(X, np.ndarray):
-			return pred.to_numpy()
+		return self.predict(X)
